2800afd7a03794de74813210be4d979526bd8aa6
vhpraneeth <praneeth.18bce7147@vitap.ac.in>
2020-11-12 06:21:24 +0000

    Improved code

--- a51.py
+++ a51.py
@@ -1,7 +1,5 @@
 
 import re
-import sys
-import copy
 
 reg_x_length = 19
 reg_y_length = 22
@@ -65,42 +63,42 @@ def get_majority(x,y,z):
 
 def get_keystream(length):
     'calculates the keystream by XOR-ing the appropriate indeces'
-    reg_x_temp = copy.deepcopy(reg_x)
-    reg_y_temp = copy.deepcopy(reg_y)
-    reg_z_temp = copy.deepcopy(reg_z)
+    reg_x_temp = reg_x.copy()
+    reg_y_temp = reg_y.copy()
+    reg_z_temp = reg_z.copy()
     keystream = []
     i = 0
     while i < length:
         majority = get_majority(reg_x_temp[8], reg_y_temp[10], reg_z_temp[10])
         if reg_x_temp[8] == majority: 
             new = reg_x_temp[13] ^ reg_x_temp[16] ^ reg_x_temp[17] ^ reg_x_temp[18]
-            reg_x_temp_two = copy.deepcopy(reg_x_temp)
+            reg_x_temp_two = reg_x_temp.copy()
             j = 1
             while(j < len(reg_x_temp)):
                 reg_x_temp[j] = reg_x_temp_two[j-1]
-                j = j + 1
+                j += 1
             reg_x_temp[0] = new
 
         if reg_y_temp[10] == majority:
             new_one = reg_y_temp[20] ^ reg_y_temp[21]
-            reg_y_temp_two = copy.deepcopy(reg_y_temp)
+            reg_y_temp_two = reg_y_temp.copy()
             k = 1
             while(k < len(reg_y_temp)):
                 reg_y_temp[k] = reg_y_temp_two[k-1]
-                k = k + 1
+                k += 1
             reg_y_temp[0] = new_one
 
         if reg_z_temp[10] == majority:
             new_two = reg_z_temp[7] ^ reg_z_temp[20] ^ reg_z_temp[21] ^ reg_z_temp[22]
-            reg_z_temp_two = copy.deepcopy(reg_z_temp)
+            reg_z_temp_two = reg_z_temp.copy()
             m = 1
             while(m < len(reg_z_temp)):
                 reg_z_temp[m] = reg_z_temp_two[m-1]
-                m = m + 1
+                m += 1
             reg_z_temp[0] = new_two
 
         keystream.insert(i, reg_x_temp[18] ^ reg_y_temp[21] ^ reg_z_temp[22])
-        i = i + 1
+        i += 1
     return keystream
 
 
@@ -110,8 +108,8 @@ def convert_binary_to_str(binary):
     length = len(binary) - 8
     i = 0
     while(i <= length):
-        s = s + chr(int(binary[i:i+8], 2))
-        i = i + 8
+        s += chr(int(binary[i:i+8], 2))
+        i += 8
     return str(s)
 
 
@@ -124,8 +122,8 @@ def encrypt(plain):
     keystream = get_keystream(len(binary))
     i = 0
     while(i < len(binary)):
-        s = s + str(binary[i] ^ keystream[i])
-        i = i + 1
+        s += str(binary[i] ^ keystream[i])
+        i += 1
     return s
 
 
@@ -138,8 +136,8 @@ def decrypt(cipher):
     i = 0
     while(i < len(cipher)):
         binary.insert(i,int(cipher[i]))
-        s = s + str(binary[i] ^ keystream[i])
-        i = i + 1
+        s += str(binary[i] ^ keystream[i])
+        i += 1
     return convert_binary_to_str(str(s))
 
 
@@ -206,7 +204,7 @@ def tha_main():
     first_choice = user_input_choice()
     if(first_choice == '0'):
         print('Have an awesome day!!!')
-        sys.exit(0)
+        exit(0)
     elif(first_choice == '1'):
         plaintext = user_input_plaintext())
         print(plaintext)
